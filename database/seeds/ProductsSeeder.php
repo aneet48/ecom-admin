200d64e02f83e3e88f9dd30d2b49dbeb87b05bc8
unknown <>
2020-06-14 00:04:04 +0530

    updates

--- database/seeds/ProductsSeeder.php
+++ database/seeds/ProductsSeeder.php
@@ -18,9 +18,17 @@ class ProductsSeeder extends Seeder
     {
         $faker = Faker\Factory::create();
         $type = ['Buy', 'Rental'];
-        $img_ids = [
-            0, 1, 10, 100, 1000, 1001, 1002, 1003, 1004, 1005, 1006, 1008, 1009, 1010, 101, 1011, 1012, 1013, 1014, 1015, 1016, 1018,
-            1020, 1021, 1022, 1023, 1024, 1025,
+        // $img_ids = [
+        //     0, 1, 10, 100, 1000, 1001, 1002, 1003, 1004, 1005, 1006, 1008, 1009, 1010, 101, 1011, 1012, 1013, 1014, 1015, 1016, 1018,
+        //     1020, 1021, 1022, 1023, 1024, 1025,
+        // ];
+        $imges = [
+            'https://images.pexels.com/photos/4319001/pexels-photo-4319001.jpeg',
+            'https://images.pexels.com/photos/3309967/pexels-photo-3309967.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+            'https://images.pexels.com/photos/1078958/pexels-photo-1078958.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            'https://images.pexels.com/photos/1435735/pexels-photo-1435735.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            'https://images.pexels.com/photos/210617/pexels-photo-210617.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            'https://images.pexels.com/photos/1884581/pexels-photo-1884581.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
         ];
 
         for ($i = 0; $i < 40; $i++) {
@@ -39,8 +47,8 @@ class ProductsSeeder extends Seeder
             ]);
 
             for ($j = 0; $j < 2; $j++) {
+                $path = $imges(array_rand($imges));
                 // $path = 'https://i.picsum.photos/id/' . $img_ids[array_rand($img_ids)] . '/600/400.jpg';
-                $path = 'https: //source.unsplash.com/300x400';
                 $filename = 'pimg_' . time() . rand(10, 999) . '.jpg';
 
                 Image::make($path)->save(public_path('storage/products/' . $filename));
